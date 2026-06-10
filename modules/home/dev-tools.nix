e6592cf6b377aa4d6264ad798b0eb37611c7ff3f
Ken Johnson <ken@onechronos.com>
2026-06-10 14:32:54 -0500

    add rust-script

--- modules/home/dev-tools.nix
+++ modules/home/dev-tools.nix
@@ -13,7 +13,7 @@ in {
       ++ lib.optionals (machine.hasRole "desktop") [pi-coding-agent]
       ++ lib.optionals config.devTools.languages.nix [alejandra nil manix statix nix-search devenv]
       ++ lib.optionals config.devTools.languages.cpp [gnumake llvm clang libiconv]
-      ++ lib.optionals config.devTools.languages.rust [capnproto capnproto-rust protobuf]
+      ++ lib.optionals config.devTools.languages.rust [capnproto capnproto-rust protobuf rust-script]
       ++ lib.optionals config.devTools.languages.zig [zig_0_15]
       ++ lib.optionals config.devTools.languages.node [nodejs_24 pnpm]
       ++ lib.optionals config.devTools.languages.python [uv python312Packages.grip]
